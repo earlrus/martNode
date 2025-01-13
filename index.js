d9789db094d847895cc32192fee4c7df8a488a81
Aman <basilcrouch10@gmail.com>
2025-01-13 13:18:41 +0530

    email verification correction

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const sendVerificationEmail = async (email, verificationToken) => {
     from: "MobileMart.com",
     to: email,
     subject: "Email Verification",
-    text: `Please click the following link to verify your email: http://localhost:8000/verify/${verificationToken}`,
+    text: `Please click the following link to verify your email: https://mart-node-49hw.vercel.app/verify/${verificationToken}`,
   };
 
   // Send the email
